8531b48fc566582ce6460660da8dda8f272785d9
SadeghHayeri <hayerisadegh@gmail.com>
2017-08-05 03:53:08 +0430

    better models!

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, DateTime, String, Integer, func, ForeignKey, Enum
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Sequence
 import enum
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq', start=1, increment=1), unique=True, primary_key=True)
 
     username = Column(String, unique=True) #TODO: fix edit username problems!
     first_name = Column(String)
@@ -15,24 +16,30 @@ class User(Base):
 
     first_access = Column(DateTime, default=func.now())
 
+    def __str__(self):
+        return '%s (%s %s)' % (self.username, self.first_name, self.last_name)
+
 
 class Note(Base):
     __tablename__ = 'note'
-    id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq', start=1, increment=1), unique=True, primary_key=True)
 
     title = Column(String)
-    body = Column(String)
+    body = Column(String, default='')
 
     last_edit_date = Column(DateTime, default=func.now())
     last_edit_user = Column(Integer, ForeignKey("user.id"))
 
-    previous_version = Column(Integer, ForeignKey("note.id"), primary_key=True, nullable=True, default=None)
-    next_version = Column(Integer, ForeignKey("note.id"), primary_key=True, nullable=True, default=None)
+    previous_version = Column(Integer, ForeignKey("note.id"), nullable=True)
+    next_version = Column(Integer, ForeignKey("note.id"), nullable=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.body, self.title)
 
 
 class Permission(Base):
     __tablename__ = 'permission'
-    id = Column(Integer, unique=True, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq', start=1, increment=1), unique=True, primary_key=True)
 
     class PT(enum.Enum):
         owner = 1
