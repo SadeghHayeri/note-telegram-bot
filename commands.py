c83da7d8a66d762bc6a31b4313fa42d970a1d3cf
SadeghHayeri <hayerisadegh@gmail.com>
2017-08-05 03:54:17 +0430

    new_note and note_list added.

--- commands.py
+++ commands.py
@@ -7,6 +7,8 @@ import logging
 import keyboards
 from sqlalchemy import create_engine
 from translator import _
+from scenarios import Scenurios
+from manager import Manager
 
 from YamJam import yamjam
 db_CFG = yamjam()['sharenote']['database']
@@ -14,7 +16,6 @@ db_CFG = yamjam()['sharenote']['database']
 class Commands:
 
     def __init__(self):
-
         # Create database session connection
         Session = sessionmaker()
         engine = create_engine('postgresql://%s:%s@%s:%d/%s' % (db_CFG['user'],
@@ -25,33 +26,76 @@ class Commands:
         Session.configure(bind=engine)
         self.session = Session()
 
+        # Create scenarios manager
+        self.scenarios = Scenurios(self, self.session)
+
         # Enable logging
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
+    def getSenarios(self):
+        return self.scenarios
+
     # /start
-    def start(self, bot, update):
-        if not self.session.query(User.id).filter(User.id == update.message.from_user.id).first():
+    def main_menu(self, bot, update):
+        from_user = update.message.from_user if update.message else update.callback_query.from_user
+        if not self.session.query(User.id).filter(User.id == from_user.id).first():
             new_user = User(
-                id=update.message.from_user.id,
-                username=update.message.from_user.username,
-                first_name=update.message.from_user.first_name,
-                last_name=update.message.from_user.last_name,
-                language=update.message.from_user.language_code
+                id=from_user.id,
+                username=from_user.username,
+                first_name=from_user.first_name,
+                last_name=from_user.last_name,
+                language=from_user.language_code
             )
             self.session.add(new_user)
             self.session.commit()
-        update.message.reply_text(_('welcome_message'), reply_markup=keyboards.start())
 
+        note_exist = False if len(Manager.get_notes(self.session, from_user.id)) == 0  else True
+        if update.message:
+            update.message.reply_text(text=_('welcome_message'),
+                                        reply_markup=keyboards.main_menu(note_exist=note_exist))
+        else:
+            query = update.callback_query
+            bot.edit_message_text(text=_('welcome_message'),
+                                    chat_id=query.message.chat_id,
+                                    message_id=query.message.message_id,
+                                    reply_markup=keyboards.main_menu(note_exist=note_exist))
 
-    def button(self, bot, update):
+###########
+    def s_new_note_0(self, bot, update):
         query = update.callback_query
-
-        bot.edit_message_text(text="Selected option: %s" % query.data,
-                                reply_markup=keyboards.start(),
+        bot.edit_message_text(text=_('get_title'),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
+
+    def s_new_note_1(self, bot, update):
+        update.message.reply_text(text=_('get_title_with_error_title'))
+###########
+
+    def show_notes(self, bot, update):
+        query = update.callback_query
+        print('show notes for user:', query.from_user.id)
+        notes = Manager.get_notes(self.session, query.from_user.id)
+        bot.edit_message_text(text=_('your_notes'),
+                                chat_id=query.message.chat_id,
+                                message_id=query.message.message_id,
+                                reply_markup=keyboards.note_list(notes))
+
+    # all buttons click
+    def button_handler(self, bot, update):
+        query = update.callback_query
+
+        if self.scenarios.checkUser(query.from_user.id):
+            self.scenarios.handler(bot, update)
+        else:
+            print('click on: ', query.data)
+            if '/new_note' in query.data:
+                self.scenarios.startScenurio(bot, update, 'new_note')
+            elif u'/my\u0640notes' in query.data:
+                self.show_notes(bot, update)
+            elif '/main_menu' in query.data:
+                self.main_menu(bot, update)
 
     # /help
     def help(self, bot, update):
